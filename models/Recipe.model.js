ee059c26ecc41aa0cbdf96d566ab68ae110c4373
“Marcela <mpmontiela@gmail.com>
2022-10-27 16:26:45 +0200

    done

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -3,6 +3,43 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  level: {
+    type: String,
+    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]
+  },
+  ingredients: {
+    type: [String]
+  },
+  cuisine: {
+    Type: String
+  },
+  dishType: {
+    Type: String 
+  },
+  image: {
+  Type: String,
+  default: "https://images.media-allrecipes.com/images/75131.jpg"
+  },
+  duration: {
+    Type: Number,
+    min: 0
+  },
+  creator: {
+    type: String
+  },
+  created: {
+    type: Date,
+    default: Today
+  }
+  
+  
+
+
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
